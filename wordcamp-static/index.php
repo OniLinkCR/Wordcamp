11801ace7f38b3dc76328535f48869f86884a644
OniLinkCR <marco.berrocal.lopez@est.una.ac.cr>
2018-07-23 12:01:39 -0600

    Initial setup of static files

---
+++ wordcamp-static/index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+	<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es-419">  <![endif]-->
+	<!--[if IE 7]>         <html class="no-js lt-ie9 ie7" lang="es-419"><![endif]-->
+	<!--[if IE 8]>         <html class="no-js lt-ie9 ie8" lang="es-419"> <![endif]-->
+	<!--[if gt IE 8]><!--> <html class="no-js " lang="es-419">         <!--<![endif]-->
+	<head>
+		<meta charset="utf-8" />
+		<title>Index</title>
+
+		<meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+		<link href="https://fonts.googleapis.com/css?family=Rufina:700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Oxygen:400,700" rel="stylesheet">
+
+		<link rel="stylesheet" href="css/style.css" />
+
+
+
+	</head>
+
+
+
+	<body>
+
+		<header>
+
+			<a href="#"><h1>WordCamp Demo</h1></a>
+
+			<nav class="main-menu">
+
+				<ul>
+					<li><a href="#">Montañas</a></li>
+					<li><a href="#">Aventura</a></li>
+					<li><a href="#">Playa</a></li>
+					<li><a href="#">Cafe</a></li>
+					<li><a href="#">Style</a></li>
+					<li><a href="#">Ciudad</a></li>
+				</ul>
+
+			</nav>
+
+
+		</header>
+
+		<!-- <div class="main slider"> -->
+
+		<ul class="slideshow">
+
+			<li class="slide" style="background-image:url('images/uvita.jpg")>
+				<h1>Punta Uvita</h1>
+				<p>La reserva marina mas grande, que fue creada en 1990, fue nombrada por sus ballenas jorobadas ya que muchas frecuentan sus aguas.</p>
+
+
+			</li>
+
+			<li class="slide" style="background-image:url('images/santa-tere.jpg")>
+				<h1>Santa Teresa</h1>
+				<p>Con vientos constantes , las mareas de Santa Teresa han atraido a cientos de surfistas de todo el mundo.</p>
+
+			</li>
+
+
+			<li class="slide" style="background-image:url('images/bajos-del-toro.jpg")>
+				<h1>Bajos del Toro</h1>
+				<p>La zona de Bajos del toro es una mina de lugares para pasear chivísimas. </p>
+
+			</li>
+
+		</ul>
+
+
+
+		<!-- </div> -->
+
+		<div class="upper-posts">
+
+			<article class="post">
+
+					<figure>
+						<img src="images/barva.jpg" alt="" />
+					</figure>
+
+					<span class="meta-data">Barva / Julio 1, 2015</span>
+					<h2>Fiestas de Barva, adrenalina 100%</h2>
+
+					<a href="#" class="button">Leer mas</a>
+
+
+			</article>
+
+			<article class="post">
+
+				<figure>
+					<img src="images/dota.jpg" alt="" />
+				</figure>
+
+				<span class="meta-data">San Jose / Julio 1, 2018</span>
+				<h2>El cafe de Dota</h2>
+
+				<a href="#" class="button">Leer mas</a>
+
+
+			</article>
+
+
+			<article class="post">
+
+				<figure>
+					<img src="images/cartago.jpg" alt="" />
+				</figure>
+
+				<span class="meta-data">Cartago / Julio 1, 2015</span>
+				<h2>Cartaguito Campeon?</h2>
+
+				<a href="#" class="button">Leer mas</a>
+
+
+			</article>
+
+
+			<article class="post">
+
+				<figure>
+					<img src="images/sarchi.jpg" alt="" />
+				</figure>
+
+				<span class="meta-data">Alajuelsssssa / Julio 1, 2015</span>
+				<h2>Sarchi, destino artesanal</h2>
+
+				<a href="#" class="button">Leer mas</a>
+
+
+			</article>
+
+
+
+		</div>
+
+		<footer>
+
+		<nav class="icons">
+
+			<ul>
+				<li><a href="#">Facebook</a></li>
+				<li><a href="#">Twitter</a></li>
+			</ul>
+
+		</nav>
+		</footer>
+		<!--upper-posts -->
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+		<!-- <script src="js/cbpBGSlideshow.js"></script> -->
+		<script src="js/slider.js"></script>
+
+
+	</body>
+
+
+</html>
